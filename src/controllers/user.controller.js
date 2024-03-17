25fc6ef41338e53b52db516148cbf6ec95fbb31a
Shivam <shivamrai6836@gmail.com>
2024-03-17 19:34:56 +0530

    User Login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,22 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/api-response.js";
 
+const generateAccessAndRefreshToken = async(userId) => {
+    try {
+        
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave : false });
+
+        return { accessToken, refreshToken };
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating tokens");
+    }
+}
 
 export const registerUser = asyncHandler (async(req, res, next)=>{
     const { fullname, username, email, password } = req.body;
@@ -64,6 +80,59 @@ export const registerUser = asyncHandler (async(req, res, next)=>{
 
     return res.status(201).json(
         new ApiResponse(200, constrainedUser, "User has been created successfully")
+    )
+
+});
+
+export const loginUser = asyncHandler(async(req, res, next)=>{
+
+    const { username, email, password } = req.json();
+
+    if (!username || !email) {
+        throw new ApiError(400, "Fields re required");
+    }
+
+    const user = await User.findOne({
+        $or : [
+            username,
+            email
+        ]
+    });
+
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+
+    const isPasswordCorrect = await user.isPasswordCorrect(password);
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(401, "Incorrect Password");
+    }
+
+    const { refreshToken, accessToken } = await generateAccessAndRefreshToken(user._id);
+    
+    const loggedInUser = {
+        _id : user._id,
+        fullname : user.fullname,
+        email : user.email,
+        avatar : user.avatar,
+        coverImage : user.coverImage,
+        watchHistory : user.watchHistory,
+        createdAt : user.createdAt,
+        updatedAt: user.updatedAt
+    }
+
+    const options  = {
+        httpOnly : true,
+        secure : true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(200, loggedInUser, "User has been loggedIn successfully")
     )
 
 });
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -63,8 +63,8 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
 
-userSchema.methods.generateAccessToken = async function () {
-    return await jwt.sign(
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
         {
             _id : this._id,
             email : this.email,
@@ -77,8 +77,8 @@ userSchema.methods.generateAccessToken = async function () {
     );
 }
 
-userSchema.methods.generateRefreshToken = async function () {
-    return await jwt.sign(
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
         {
             _id : this._id,
         },
